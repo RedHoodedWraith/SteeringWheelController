dcfc7cd285edfb1538cccfb14ac6a062af995bc4
Asami De Almeida <stem@wraith.red>
2022-06-13 00:47:52 +1200

    Starter Code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# SteeringWheelController
+# Steering Wheel Controller
+
+This project is my attempt at making a Steering Wheel Control adaptor for my car.
+
+The car this code is being designed for is the Ford Focus 2007.
+
+The code is based on the one found [here](https://www.circuitbasics.com/arduino-ohm-meter/).
+
+# Observed Resistance Values from Vehicle
+
+| Vehicle Steering Wheel Control | Kilo-Ohms |
+|--------------------------------|-----------|
+| Volume Up                      | 0.149     |
+| Volume Down                    | 0.0565    |
+| Mode                           | 1.039     |
+| Seek+                          | 0.3029    |
+| Seek-                          | 0.563     |
+| Idle (no button pushes)        | 5.050     |
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
+#include "SWC_Resistance_Values.h"
 
-void setup() {
-// write your initialization code here
+int signal_pin = 0;
+int raw = 0;
+int supplied_voltage = 5;
+float manipulated_voltage = 0;
+float CONTROL_RES = CONTROL_RES_VAL;
+float swc_val = 0;
+float buffer = 0;
+
+void reportReadings() {
+    Serial.print("manipulated_voltage: ");
+    Serial.println(manipulated_voltage);
+    Serial.print("swc_val: ");
+    Serial.println(swc_val);
 }
 
-void loop() {
-// write your code here
+void updateReadings() {
+    raw = analogRead(signal_pin);
+    buffer = raw * supplied_voltage;
+    manipulated_voltage = (buffer) / 1024;
+    buffer = (supplied_voltage / manipulated_voltage) - 1;
+    swc_val = CONTROL_RES * buffer;
+}
+
+void setup(){
+    Serial.begin(9600);
+}
+
+void loop(){
+    updateReadings();
+    reportReadings();
+    delay(1000);
 }
